e4da6823ec07cbf8480683054e998266eaa9173a
Justin Sun <justin.sun@fdmgroup.com>
2024-01-16 08:41:14 +0000

    Fixed the double nav bar

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -6,11 +6,11 @@ import BankAccountCard from "../components/BankAccountCard";
 import CardDisplay from "../components/CardDisplay";
 import Navbar from "../components/Navbar";
 import BankTransactionHistory from "../components/BankTransactionHistory";
-import { useEffect, useState, useCallback } from 'react'
-import { NavLink, Navigate } from 'react-router-dom'
-import { useAuth } from '../misc/AuthContext';
-import { bankingApi } from '../misc/BankingApi';
-import { handleLogError } from '../misc/Helpers';
+import { useEffect, useState, useCallback } from "react";
+import { NavLink, Navigate } from "react-router-dom";
+import { useAuth } from "../misc/AuthContext";
+import { bankingApi } from "../misc/BankingApi";
+import { handleLogError } from "../misc/Helpers";
 
 const transactionData = [
   {
@@ -78,21 +78,27 @@ const HomePage = () => {
         setUserDb(userResponse.data);
 
         if (userResponse.data && userResponse.data.bankAccount) {
-          const bankResponse = await bankingApi.getTransactions(userResponse.data.bankAccount.id);
+          const bankResponse = await bankingApi.getTransactions(
+            userResponse.data.bankAccount.id
+          );
           console.log(bankResponse.data);
 
           // Transform the bank data into the format you want for transactions
-          const formattedTransactions = bankResponse.data.map(transaction => ({
-            id: transaction.id,
-            description: transaction.description,
-            amount: transaction.amount,
-            bankAccountId: transaction.bankAccount.id,
-            date: transaction.createdAt,
-          }));
+          const formattedTransactions = bankResponse.data.map(
+            (transaction) => ({
+              id: transaction.id,
+              description: transaction.description,
+              amount: transaction.amount,
+              bankAccountId: transaction.bankAccount.id,
+              date: transaction.createdAt,
+            })
+          );
 
           setTransactions(formattedTransactions);
         } else {
-          console.error('User data or bank account information is not available.');
+          console.error(
+            "User data or bank account information is not available."
+          );
         }
       } catch (error) {
         handleLogError(error);
@@ -111,20 +117,18 @@ const HomePage = () => {
     return <Navigate to="/login" />;
   }
 
-  return ( userDb &&
-    <div className="HomePage">
-      <div className="left-column">
-        <Navbar />
-      </div>
-
-      <div className="right-column">
+  return (
+    userDb && (
+      <div className="home-page">
         <div className="top">
           {appliedToCreditCard ? (
             () => null
           ) : (
             <div className="credit-apply-div">
               <NavLink to="/application">
-                <button className="credit-apply-btn">Apply To Credit Card</button>
+                <button className="credit-apply-btn">
+                  Apply To Credit Card
+                </button>
               </NavLink>
             </div>
           )}
@@ -189,7 +193,7 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
@@ -200,4 +204,3 @@ export default HomePage;
 // we  will show each card
 // when we pick, we change to the currentCard State
 // we can show data for current card
-
